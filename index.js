59a69a3f1b9787dd753c8d2b2a87b34f743a7d7f
Marley Watson <marleykw17@gmail.com>
2022-07-25 09:34:38 -0400

    commit tree 75c6be1f7f81f45ed4cdef5a27fb953f35d21ddc

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats  =  ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+function appendCat(name) {
+    const appendCat = [...cats, name];
+    return appendCat;
+    return cats;
+}
+
+function prependCat(name) {
+    const prependCat = [name, ...cats];
+    return prependCat;
+    return cats;
+}
+
+function removeLastCat(name) {
+    const removeLastCat = [...cats.slice(0, cats.length - 1)];
+    return removeLastCat;
+    return cats;
+}
+
+function removeFirstCat(name) {
+    const removeFirstCat = [...cats.slice(1, cats.length)];
+    return removeFirstCat;
+    return cats;
+}
